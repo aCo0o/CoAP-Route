89c9b836da7405ac2fd8bc68864ab249d22209e5
a.Cincarevic <acoaco908@gmail.com>
2016-06-27 22:41:09 +0200

    Update please.js

--- please.js
+++ please.js
@@ -8,7 +8,7 @@ module.exports = {
 
 		console.log("GET");
 		theResponse = "";
-    theResponse += "\n" +this.allPosts.length + " posts\nWith the content: " + this.allPosts;
+    	theResponse += "\n" +this.allPosts.length + " posts\nWith the content: " + this.allPosts;
 		return res.end(theResponse);
 	},
 	handleThePost: function(req, res){
@@ -17,19 +17,19 @@ module.exports = {
 		this.allPosts.push(req.payload);
 		console.log("POST - Payload: " + req.payload);
 		theResponse = "";
-	  theResponse += "\n[This was a POST: " + req.payload + "]\n\n" + this.allPosts.length + " posts\nWith the content: " + this.allPosts;
-		return res.end(TheResponse);
+	  	theResponse += "\n[This was a POST: " + req.payload + "]\n\n" + this.allPosts.length + " posts\nWith the content: " + this.allPosts;
+		return res.end(theResponse);
 
 	},
 	handleThePut: function(req, res){
 
-    //deletes last post
+    	//deletes last post
 		this.allPosts.pop();
 		//push in the payload
-    this.allPosts.push(req.payload);
-    console.log("PUT - Payload: " + req.payload);
-    theResponse = "";
-    theResponse += "\n[This was a PUT: " + req.payload + "]\n\n" + this.allPosts.length + " posts\nWith the content: " + this.allPosts;
+    	this.allPosts.push(req.payload);
+    	console.log("PUT - Payload: " + req.payload);
+    	theResponse = "";
+    	theResponse += "\n[This was a PUT: " + req.payload + "]\n\n" + this.allPosts.length + " posts\nWith the content: " + this.allPosts;
 		return res.end(theResponse);
 	},
 	handleTheDelete: function(req, res){
@@ -43,7 +43,7 @@ module.exports = {
 	doNot: function(req, res){
 
 		console.log("!routeBlock - " + req.method + " : on", req.url);
-		return res.end(theText + "\n\n" + "ALL ROUTES EXCEPT '/' IS BLOCKED");
+		return res.end("ALL ROUTES EXCEPT '/' IS BLOCKED");
 	}
 
 };
